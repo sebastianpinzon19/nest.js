fae8d1cd52294d6c8f61c188342eada93fb6e85e
sebastianpinzon19 <152411485+sebastianpinzon19@users.noreply.github.com>
2024-11-13 19:01:51 -0500

    feat(back-end)nest productos

--- proyecto_nest.js/sistema_gestor_nest/src/module/app.module.ts
+++ proyecto_nest.js/sistema_gestor_nest/src/module/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProveedoresModule } from './proveedores/proveedores.module';
+import { ProductosModule } from './productos/productos.module';
 
 @Module({
-  imports: [ProveedoresModule],
+  imports: [ProveedoresModule, ProductosModule],
   controllers: [AppController],
   providers: [AppService],
 })
