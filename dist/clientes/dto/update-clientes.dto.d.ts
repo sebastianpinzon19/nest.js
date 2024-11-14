7b122e486979cc0e541438722590873ed6c3369d
sebastianpinzon19 <152411485+sebastianpinzon19@users.noreply.github.com>
2024-11-13 21:40:52 -0500

    resolviendo problemas

--- dist/clientes/dto/update-clientes.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class UpdateClientesDto {
-    numero_identificacion?: string;
-    nombre_cliente?: string;
-    email_cliente?: string;
-    celular_cliente?: string;
-    activo_cliente?: boolean;
-}
--- dist/productos/dto/update-productos-dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { Clientes } from 'src/module/clientes/schema/cliente.schema';
-export declare class UpdateProductosDto {
-    nombre_producto?: string;
-    cantidad?: number;
-    precio?: number;
-    proveedor?: string[];
-    cliente?: Clientes[];
-    activo?: boolean;
-}
